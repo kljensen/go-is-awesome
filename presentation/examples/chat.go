cc17d39971b710e447bd9f15e0e378f8b5a706ab
Kyle L. Jensen <kljensen@gmail.com>
2013-04-23 16:44:38 -0400

    Update chat example…code for doing with group afterward.

---
+++ presentation/examples/chat.go
@@ -0,0 +1,44 @@
+/*
+	Chat example, originally from 
+	http://synflood.at/tmp/golang-slides/mrmcd2012.html#27
+*/
+package main
+
+import (
+	"log"
+	"net"
+	// "os"
+)
+
+func handleConnection(c net.Conn) {
+	buf := make([]byte, 4096)
+
+	for {
+		n, err := c.Read(buf)
+		if err != nil || n == 0 {
+			c.Close()
+			break
+		}
+		n, err = c.Write(buf[0:n])
+		if err != nil {
+			c.Close()
+			break
+		}
+	}
+	log.Printf("Connection from %v closed.", c.RemoteAddr())
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":6000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		go handleConnection(conn)
+	}
+}
