600ab44d052ff86c6b47ca2764a21576f90a3f03
Kyle L. Jensen <kljensen@gmail.com>
2013-04-23 16:42:58 -0400

    Show alternative initialization in maps and slices example

--- presentation/examples/maps.go
+++ presentation/examples/maps.go
@@ -1,14 +1,19 @@
 package main
+
 import "fmt"
 
 func main() {
 
-	var timeZone = map[string] int {
-	    "UTC":  0,
-	    "EST": -5,
-	    "CST": -6,
-	    "MST": -7,
-	    "PST": -8,
+	var timeZone = map[string]int{
+		"UTC": 0,
+		"EST": -5,
+		"CST": -6,
+		"MST": -7,
+		"PST": -8,
 	}
 	fmt.Println(timeZone["EST"])
+
+	timeZone2 := make(map[string]int)
+	timeZone2["XST"] = 4
+	fmt.Println(timeZone2["XST"])
 }
--- presentation/examples/slices.go
+++ presentation/examples/slices.go
@@ -1,17 +1,19 @@
 package main
+
 import "fmt"
 
 func main() {
 
 	// Array, fixed size
-	dayArray := [7]int {1, 2, 3, 4, 5, 6, 7}
-    fmt.Println(dayArray)
+	dayArray := [7]int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(dayArray)
 
-    // Slice, flexible size
-    daySlice := make([]int, 7)
-    for i, day := range dayArray{
-        daySlice[i] = day
-    }
-    daySlice = append(daySlice, 121, 122, 123, 124)
-    fmt.Println(daySlice)
+	// Slice, flexible size
+	daySlice := make([]int, 7)
+	for i, day := range dayArray {
+		daySlice[i] = day
+	}
+	// daySlice := []int{1, 2, 3, 4, 5, 6, 7} // <- Alternative
+	daySlice = append(daySlice, 121, 122, 123, 124)
+	fmt.Println(daySlice)
 }
