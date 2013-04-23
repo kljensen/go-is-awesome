ffeae4e282f52c0cbbb15b3ad3b8b378c92b78a1
Kyle L. Jensen <kljensen@gmail.com>
2013-04-23 09:57:59 -0400

    Clean up goroutine example

--- presentation/examples/goroutines.go
+++ presentation/examples/goroutines.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func say(s string) {
 	for i := 0; i < 5; i++ {
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(250)))
 		fmt.Println(s, i)
 	}
 }
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	go say("foo")
+	go say("woot")
 	go say("bar")
 	time.Sleep(5 * time.Second)
 }
